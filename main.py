bd62eb7ec5ad3c2e0082d85bf69e992d06037c9e
Imperium34 <ali.ari3443@gmail.com>
2025-10-07 15:15:43 +0300

    added latter counter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import get_word_count
+from stats import get_character_count
 
 def main():
     get_word_count("books/frankenstein.txt")
+    get_character_count("books/frankenstein.txt")
 main()
--- stats.py
+++ stats.py
@@ -2,8 +2,8 @@ def get_book_text(filepath):
     with open(filepath) as f:
         text = f.read()
         text = text.split()
-        for word in text:
-            word = word.lower()
+        for i in range(0, len(text)):
+            text[i] = text[i].lower()
         return text
     
 def get_word_count(filepath):
@@ -13,4 +13,14 @@ def get_word_count(filepath):
 
 def get_character_count(filepath):
     words = get_book_text(filepath)
-    words = words.lower()
+    character_count = {}
+    for word in words:
+        letters = list(word)
+        for letter in word:
+            character_count[letter] = 0
+
+    for word in words:
+        letters = list(word)
+        for letter in word:
+            character_count[letter] += 1
+    print(character_count)
